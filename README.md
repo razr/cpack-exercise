e567c5de2e70a6d7923096365185e9e8a83f6bc1
Andrei Kholodnyi <andrei.kholodnyi@windriver.com>
2024-06-27 12:56:32 +0200

    add CPack

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # cpack-exercise
 
+There are two deb packages built: Runtime and Development
+
 ```bash
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX=/usr
-cmake --build build
-DESTDIR=$HOME/tmp cmake -DCOMPONENT=runtime -P build/cmake_install.cmake
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build .
+make list_install_components
+cpack -G DEB
+cpack -G DEB -D CPACK_COMPONENTS_ALL=Runtime
+cpack -G DEB -D CPACK_COMPONENTS_ALL=Development
+find . -name "*.deb" -exec sh -c 'echo "Contents of {}:"; dpkg -c {}' \;
+DESTDIR=$HOME/tmp cmake -DCOMPONENT=Runtime -P build/cmake_install.cmake
 ```
---
+++ world_lib/cmake/packaging/CPackConfig.cmake
@@ -0,0 +1,37 @@
+# Include CPack configuration
+include(InstallRequiredSystemLibraries)
+
+set(CPACK_PACKAGE_NAME "world")
+set(CPACK_PACKAGE_VERSION "1.0.0")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "World Library")
+set(CPACK_PACKAGE_VENDOR "Your Name")
+set(CPACK_PACKAGE_CONTACT "your.email@example.com")
+set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}")
+
+
+# Specify the generator for Debian packages
+set(CPACK_GENERATOR "DEB")
+
+# Set Debian package specific settings
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "your.email@example.com") # Required
+set(CPACK_DEBIAN_PACKAGE_SECTION "libs")
+set(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
+set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION}")
+set(CPACK_DEBIAN_PACKAGE_RELEASE "1")
+set(CPACK_DEBIAN_ARCHITECTURE ${CMAKE_SYSTEM_PROCESSOR})
+
+set(CPACK_DEB_COMPONENT_INSTALL ON)
+set(CPACK_DEBIAN_PACKAGE_DEPENDS ".")
+
+include(CPack)
+# Define components
+cpack_add_component(Runtime
+    DISPLAY_NAME "Runtime"
+    DESCRIPTION "World Library Runtime"
+)
+
+cpack_add_component(Development
+    DISPLAY_NAME "Development"
+    DESCRIPTION "World Library Development Files"
+    DEPENDS Runtime
+)
